2d5392a7c1c695c34d2a40364d8f0e69ddd64704
Belekko <m.u.belekpr@gmail.com>
2022-05-28 19:55:36 +0600

    onSubmitValidationFinished

--- src/components/BasicForm.js
+++ src/components/BasicForm.js
@@ -4,21 +4,73 @@ const BasicForm = (props) => {
   const [isFirstNameValid, setIsFirstNameValid] = useState(false);
   const [isFirstNameTouched, setIsFirstNameTouched] = useState(false);
   const firstNameRef = useRef("");
+
+  const [isLastNameValid, setIsLastNameValid] = useState(false);
+  const [isLastNameTouched, setIsLastNameTouched] = useState(false);
+  const lastNameRef = useRef("");
+
+  const [isEmailValid, setIsEmailValid] = useState(false);
+  const [isEmailTouched, setIsEmailTouched] = useState(false);
+  const emailRef = useRef("");
+
   const submitHandler = (e) => {
     e.preventDefault();
+
     setIsFirstNameTouched(true);
-    const isFirstNameValueValid = firstNameRef.current.value.trim() !== "";
-    if (isFirstNameValueValid) {
+    setIsLastNameTouched(true);
+    setIsEmailTouched(true);
+
+    let firstNameValue = firstNameRef.current.value;
+    let lastNameValue = lastNameRef.current.value;
+    let emailValue = emailRef.current.value;
+
+    if (firstNameValue.trim() !== "") {
       setIsFirstNameValid(true);
     }
+
+    if (lastNameValue.trim() !== "") {
+      setIsLastNameValid(true);
+    }
+
+    if (emailValue.trim() !== "") {
+      setIsEmailValid(true);
+    }
+
+    const isFormInvalid =
+      firstNameValue.trim() === "" ||
+      lastNameValue.trim() === "" ||
+      emailValue.trim() === "";
+
+    if (isFormInvalid) {
+      return;
+    }
+    console.log("first");
+
+    firstNameValue = "";
+    setIsFirstNameTouched(false);
+    setIsFirstNameValid(false);
+
+    lastNameValue = "";
+    setIsLastNameTouched(false);
+    setIsLastNameValid(false);
+
+    emailValue = "";
+    setIsEmailTouched(false);
+    setIsEmailValid(false);
   };
 
-  const firstNameClasses =
-    isFirstNameValid && isFirstNameTouched
-      ? "form-control"
-      : "form-control invalid";
+  const firstNameInvalid = !isFirstNameValid && isFirstNameTouched;
+  const firstNameClasses = firstNameInvalid
+    ? "form-control invalid"
+    : "form-control";
 
-  const isFormValid = !isFirstNameValid;
+  const lastNameInvalid = isLastNameTouched && !isLastNameValid;
+  const lastNameClasses = lastNameInvalid
+    ? "form-control invalid"
+    : "form-control";
+
+  const emailInvalid = isEmailTouched && !isEmailValid;
+  const emailClasses = emailInvalid ? "form-control invalid" : "form-control";
 
   return (
     <form onSubmit={submitHandler}>
@@ -26,20 +78,27 @@ const BasicForm = (props) => {
         <div className={firstNameClasses}>
           <label htmlFor="name">First Name</label>
           <input ref={firstNameRef} type="text" id="name" />
+          {firstNameInvalid && (
+            <p className="error-text">First Name must not be empty!</p>
+          )}
         </div>
-        <div className="form-control">
+        <div className={lastNameClasses}>
           <label htmlFor="name">Last Name</label>
-          <input type="text" id="name" />
+          <input type="text" id="name" ref={lastNameRef} />
+          {lastNameInvalid && (
+            <p className="error-text">Last Name must not be empty!</p>
+          )}
         </div>
       </div>
-      <div className="form-control">
+      <div className={emailClasses}>
         <label htmlFor="name">E-Mail Address</label>
-        <input type="text" id="name" />
+        <input type="text" id="name" ref={emailRef} />
+        {emailInvalid && (
+          <p className="error-text">Email must include '@' or not be empty!</p>
+        )}
       </div>
       <div className="form-actions">
-        <button type="submit" disabled={!isFormValid}>
-          Submit
-        </button>
+        <button type="submit">Submit</button>
       </div>
     </form>
   );
